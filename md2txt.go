7b9a294e267f380f082b0eb49f8552f12ab5f9c8
ggaaooppeenngg <471160171@qq.com>
2015-03-27 10:15:08 +0800

    refactor move interface and corresponding structs into seperate file.

--- md2txt.go
+++ md2txt.go
@@ -1,11 +1,8 @@
 package md2txt
 
 import (
-	"bytes"
 	"regexp"
 	"unicode/utf8"
-
-	"github.com/ggaaooppeenngg/md2txt/kind"
 )
 
 const (
@@ -16,91 +13,6 @@ const (
 	tab    = "\t"
 	sapce4 = "    s"
 )
-
-type Element interface {
-	Type() kind.Kind
-	Content() []byte
-}
-
-// Head represents element beginning with '#'
-type Head struct {
-	level   int // head type h1,h2,...h6
-	content []byte
-}
-
-func (h Head) Content() []byte { return h.content }
-func (h Head) Type() kind.Kind { return kind.Head }
-
-// Paragraph represents paragraph.
-type Paragraph struct {
-	content []byte
-}
-
-func (p Paragraph) Content() []byte { return p.content }
-func (p Paragraph) Type() kind.Kind { return kind.Paragraph }
-
-// BlockQuote represents element beginning with '>'
-type BlockQuote struct {
-	Level   int // level of recursive layer
-	Content string
-}
-
-// List represents element beginning with '*'|'+'|'-'|digit
-type List struct {
-	level int // recursive level
-	items []*Item
-}
-
-func (l List) Type() kind.Kind { return kind.List }
-
-// TODO:handle sub elements
-func (l List) Content() []byte {
-	var output [][]byte
-	for _, v := range l.items {
-		output = append(output, v.content)
-	}
-	return bytes.Join(output, []byte("\n"))
-}
-
-// list item.
-type Item struct {
-	content []byte
-	subEles []Element
-}
-
-// CodeBlock represents element beginning with one tab or at least a 4 spaces.
-type CodeBlock struct {
-	level   int // recursive level
-	content []byte
-}
-
-func (c CodeBlock) Content() []byte { return c.content }
-func (c CodeBlock) Type() kind.Kind { return kind.CodeBlock }
-
-// Rule represents horizontal rules
-type Rule struct {
-}
-
-type Link struct {
-	Text  string
-	Title string
-	URL   string
-}
-
-type Emphasis struct {
-	Content string
-}
-type Image struct {
-	Text  string
-	Title string
-	Link  string
-}
-
-type InlineHTML struct {
-}
-
-type EOF struct {
-}
 
 type Position struct {
 	Row    int
